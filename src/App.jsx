4481d43e3a8f17823d5f2cbde7da490ba1974ebf
jamesAkash <akash.james@outlook.com>
2023-09-06 20:13:06 +0530

    adding items over

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -5,11 +7,12 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState(initialItems);
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <ShoppingList />
+      <Form items={items} setItems={setItems} />
+      <ShoppingList items={items} />
       <Stats />
     </div>
   );
@@ -19,27 +22,49 @@ function Logo() {
   return <h1>Shop it!🛒</h1>;
 }
 
-function Form() {
+function Form({ setItems, items }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text) {
+      const newId = items.length + 1;
+      const newItem = {
+        id: newId,
+        description: text,
+        quantity: amt,
+        packed: false,
+      };
+      setItems([...items, newItem]);
+    }
+  };
+
+  const [text, setText] = useState("");
+  const [amt, setAmt] = useState(1);
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What to buy? 🔥</h3>
-      <select>
+      <select value={amt} onChange={(e) => setAmt(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
+          <option key={num}>{num}</option>
         ))}
       </select>
-      <input type="text" placeholder="Item.." />
-      <button>Add</button>
+      <input
+        name="itemName"
+        type="text"
+        value={text}
+        placeholder="Item.."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
   );
 }
-function ShoppingList() {
+function ShoppingList({ items }) {
+  console.log(items);
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item key={item.id} {...item} />
         ))}
       </ul>
